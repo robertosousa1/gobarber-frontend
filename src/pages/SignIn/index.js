1d704cadac1756db19b75178b950f7d5e685e5dc
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-10 15:43:48 -0300

    criado página para realizar login

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
+
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
+}
