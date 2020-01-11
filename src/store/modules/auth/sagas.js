72ef57ffb30ae44690e1fba3b0167198666e9bed
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-11 16:30:13 -0300

    criado saga para signIn

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,27 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/service/history';
+import api from '~/service/api';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('usuário não é prestador');
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
