411c8010b68c474074bf3de1795a627a83b35b37
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-11 13:32:54 -0300

    usando lib yup nos formularios

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatória'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('A senha é obrigatória'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form>
-        <Input placeholder="Nome completo" />
-        <Input type="email" placeholder="Seu e-mail" />
-        <Input type="password" placeholder="Sua senha secreta" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já possui conta?</Link>
