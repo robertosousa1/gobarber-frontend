9a4c40437da182a3e12875f6468d4680782c1fe6
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-12 22:25:02 -0300

    usando toast nas mensagens de erro

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/service/history';
 import api from '~/service/api';
@@ -17,13 +18,16 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('usuário não é prestador');
+      toast.error('Usuário não é prestador');
+      yield put(signFailure());
+      return;
     }
 
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados.');
     yield put(signFailure());
   }
 }
