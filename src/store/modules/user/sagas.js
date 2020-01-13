cea93fbb303a6d4be340310802b9cda99a18d078
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-13 15:13:06 -0300

    realizando update do profile do user na api

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,6 +1,32 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-// import history from '~/service/history';
-// import api from '~/service/api';
+import api from '~/service/api';
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', () => {})]);
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso!');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    console.tron.log('bb 04');
+
+    toast.success('Erro ao atualizar perfil, confira seus dados!');
+
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
